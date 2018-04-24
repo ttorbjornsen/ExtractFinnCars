71207d4696975006e7cf66c62f6e5a4b0602905d
torbjorn.torbjornsen <ttorbjornsen@gmail.com>
2018-04-24 09:30:28 +0200

    Refactoring away kafka

--- src/main/scala/ttorbjornsen/ExtractFinnCars.scala
+++ src/main/scala/ttorbjornsen/ExtractFinnCars.scala
@@ -14,13 +14,6 @@ import scala.util.{Failure, Success}
 
 object ExtractFinnCars extends App {
   //Thread.sleep(20000) //sleep while spark docker container is getting ready. Should be moved to dockerfile.
-  val topic = "acq_car_header"
-  val brokers = "kafka:9092"
-  val props = new Properties()
-  props.put("metadata.broker.list", brokers)
-  props.put("serializer.class", "kafka.serializer.StringEncoder")
-  props.put("producer.type", "async")
-  props.put("request.required.acks", "1")
 
   import java.sql.{Connection, DriverManager, ResultSet};
 
@@ -29,15 +22,8 @@ object ExtractFinnCars extends App {
   val username = System.getProperty("username")
   val conn_str = "jdbc:postgresql://localhost:5432/finncars_db?user=" + username + "&password=" + password
 
-  // Load the driver
-  classOf[org.postgresql.Driver]
-
   // Setup the connection
   val conn = DriverManager.getConnection(conn_str)
-
-
-  val config = new ProducerConfig(props)
-  val producer = new Producer[String, String](config)
 
   System.setProperty("javax.net.ssl.trustStore", "c:\\temp\\finn-ssl-certificate.jks") //https://stackoverflow.com/questions/7744075/how-to-connect-via-https-using-jsoup
 
@@ -45,16 +31,11 @@ object ExtractFinnCars extends App {
   val hdrPages1 = Range(1,100,1)
   val hdrPages2 = Range(100,200,1)
 
-//  val hdrPages1 = Range(1,150,1)
-//  val hdrPages2 = Range(150,300,1)
-
-
   val f1:Future[Unit] = Future{
     hdrPages1.map{page =>
-      //val acqCarHeaders = Utility.scrapeCarHeaders("http://m.finn.no/car/used/search.html?year_from=2003&year_to=2003&body_type=4",1,8)
       val url = "https://www.finn.no/car/used/search.html?year_from=2007&body_type=4&rows=100&page=" + page
-      Utility.saveFinnCarsPageResults(producer, topic, url, conn)
-      println("Page " + page + " written to Kafka topic " + topic + " . Time : " + LocalDateTime.now().toString)
+      Utility.saveFinnCarsPageResults(url, conn)
+      println("Page " + page + " written to postgres " + " . Time : " + LocalDateTime.now().toString)
       Thread.sleep(5000)
     }
   }
@@ -62,8 +43,8 @@ object ExtractFinnCars extends App {
   val f2:Future[Unit] = Future{
     hdrPages2.map{page =>
       val url = "https://www.finn.no/car/used/search.html?year_from=2007&body_type=4&rows=100&page=" + page
-      Utility.saveFinnCarsPageResults(producer, topic, url, conn)
-      println("Page " + page + " written to Kafka topic " + topic + " . Time : " + LocalDateTime.now().toString)
+      Utility.saveFinnCarsPageResults(url, conn)
+      println("Page " + page + " written to postgres " + " . Time : " + LocalDateTime.now().toString)
       Thread.sleep(5000)
     }
   }
@@ -79,14 +60,12 @@ object ExtractFinnCars extends App {
   fSer2.onComplete{
     case Success(_) =>  {
       println("Finished trying to write pages " + Math.min(hdrPages1.min, hdrPages2.min) + " - " + Math.max(hdrPages1.max, hdrPages2.max) + " to Kafka")
-      producer.close()
       conn.close()
     }
 
     case Failure(_) => {
       println("Timeout - not able to complete writing to Kafka within time limit.")
       conn.close()
-      producer.close()
     }
 
   }
